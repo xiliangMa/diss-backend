2b5e6825528ca929039a3ed938b58cd3c29d3b9b
xiliangMa <xiliangMa@outlook.com>
2019-11-18 11:02:23 +0800

    update  app config
    Signed-off-by: xiliangMa <xiliangMa@outlook.com>

--- sysinit/initdb.go
+++ sysinit/initdb.go
@@ -12,29 +12,29 @@ import (
 
 func initDB() {
 
-	dbType := beego.AppConfig.String("db::db_type")
+	dbType := beego.AppConfig.String("db::Type")
 
 	//连接名称
-	dbAlias := beego.AppConfig.String(dbType + "::db_alias")
+	dbAlias := beego.AppConfig.String(dbType + "::Alias")
 
 	//数据库名称
-	//dbName := beego.AppConfig.String(dbType + "::db_name")
+	//dbName := beego.AppConfig.String(dbType + "::Name")
 	dbName := os.Getenv("MARIADB_DATABASE")
 
 	//数据库连接用户名
-	//dbUser := beego.AppConfig.String(dbType + "::db_user")
+	//dbUser := beego.AppConfig.String(dbType + "::User")
 	dbUser := os.Getenv("MARIADB_USER")
 
 	//数据库连接用户名
-	//dbPwd := beego.AppConfig.String(dbType + "::db_pwd")
+	//dbPwd := beego.AppConfig.String(dbType + "::Pwd")
 	dbPwd := os.Getenv("MARIADB_PASSWORD")
 
 	//数据库IP（域名）
-	//dbHost := beego.AppConfig.String(dbType + "::db_host")
+	//dbHost := beego.AppConfig.String(dbType + "::Host")
 	dbHost := os.Getenv("MARIADB_HOST")
 
 	//数据库端口
-	//dbPort := beego.AppConfig.String(dbType + "::db_port")
+	//dbPort := beego.AppConfig.String(dbType + "::Port")
 
 	datasource := fmt.Sprintf("%s%s%s%s%s%s%s%s", dbUser, ":", dbPwd, "@tcp(", dbHost, ":3306)/", dbName, "?charset=utf8")
 
@@ -45,7 +45,7 @@ func initDB() {
 	//orm.RegisterDataBase("default", "mysql", "root:abc123@tcp(127.0.0.1:3306)/uranus_local?charset=utf8")
 
 	//如果是开发模式，则显示命令信息
-	isDev := (beego.AppConfig.String("runmode") == "dev")
+	isDev := (beego.AppConfig.String("RunMode") == "dev")
 
 	// true: drop table 后再建表
 	force := false
