d54f421abd8f4412a15a517a1e5eeb583ca7e26e
xiliangMa <xiliangma@outlook.com>
2020-02-27 10:57:39 +0800

    api: udate api params type from query to body for securitypolicy  bmtcontroller
    Signed-off-by: xiliangMa <xiliangma@outlook.com>

--- controllers/securitypolicy/bmtcontroller.go
+++ controllers/securitypolicy/bmtcontroller.go
@@ -1,6 +1,7 @@
 package securitypolicy
 
 import (
+	"encoding/json"
 	"github.com/astaxie/beego"
 	"github.com/xiliangMa/diss-backend/models"
 )
@@ -13,7 +14,7 @@ type BMTController struct {
 // @Title GetBMTList
 // @Description Get BenchMarkTemplate List
 // @Param token header string true "authToken"
-// @Param name query string "" false "name"
+// @Param body body models.BenchMarkTemplate false "基线模版"
 // @Param from query int 0 false "from"
 // @Param limit query int 20 false "limit"
 // @Success 200 {object} models.Result
@@ -22,7 +23,7 @@ func (this *BMTController) GetBMTList() {
 	limit, _ := this.GetInt("limit")
 	from, _ := this.GetInt("from")
 	benchMarkTemplate := new(models.BenchMarkTemplate)
-	benchMarkTemplate.Name = this.GetString("name")
+	json.Unmarshal(this.Ctx.Input.RequestBody, &benchMarkTemplate)
 	this.Data["json"] = benchMarkTemplate.List(from, limit)
 	this.ServeJSON(false)
 
