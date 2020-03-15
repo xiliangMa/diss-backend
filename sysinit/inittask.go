69650148418c39c5b44f40da521c05f760b63fa9
xiliangMa <xiliangma@outlook.com>
2020-03-15 14:34:32 +0800

    mvove k8s sync spec to app.conf
    Signed-off-by: xiliangMa <xiliangma@outlook.com>

--- sysinit/inittask.go
+++ sysinit/inittask.go
@@ -1,30 +1,25 @@
 package sysinit
 
 import (
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 	uuid "github.com/satori/go.uuid"
 	"github.com/xiliangMa/diss-backend/service/task"
 	"os"
 )
 
-const (
-	SyncK8sSpec = "0 */10 * * * ?"
-)
-
 func InitTask() {
-
 	go func() {
 		th := task.NewTaskHandler()
-		// k8s
+		// 启动 k8s 任务
 		uid, _ := uuid.NewV4()
-		if err := th.AddByFunc(uid.String(), SyncK8sSpec, task.SyncAll); err != nil {
+		if err := th.AddByFunc(uid.String(), beego.AppConfig.String("k8s::SyncSpec"), task.SyncAll); err != nil {
 			logs.Error("error to add TaskHandler task: %s", err)
 			os.Exit(-1)
 		} else {
-			// to do 添加任务记录
+			// to do 添加任务到数据库管理
 		}
 		th.Start()
 		select {}
 	}()
-
 }
