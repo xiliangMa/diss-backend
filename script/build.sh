63e4bff17868ea61f69bf74dd214353e5e6e624a
fxdeco <fengjin@uranus.io>
2020-03-18 20:48:36 +0800

    build.sh basic struct

--- script/build.sh
+++ script/build.sh
@@ -1,34 +1,61 @@
 #!/bin/sh
+
+#### 初始化变量和目录
 echo "=========== 0. build start........ ==========="
-
-# 编译二进制文件
-echo "=========== 1. buid  diss-backen ==========="
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/diss-backend
-
-#清除缓存
-echo "=========== 2. remove cache ==========="
-rm -rf build/*
+## 脚本所在目录
+SCRIPT_FOLDER=$(cd "$(dirname "$0")";pwd)
+PROJECT_DIR="$SCRIPT_FOLDER/.."
+BUILD_DIR="$SCRIPT_FOLDER/../build"
 
 
-# 准备 build 文件
-echo "=========== 3. cp build file ==========="
-cp -r conf swagger bin build
-cp entrypoint.sh bin/diss-backend script/install.sh build
-cp docker-compose-prod.yml build/docker-compose.yml
+#### 清除缓存
+echo "=========== 1. remove cache ==========="
+if [ -d $BUILD_DIR ]
+then
+  rm -rf "$BUILD_DIR"
+fi
 
-#停止容器
+
+#### 编译二进制文件
+echo "=========== 2. build  diss-backen bin ==========="
+mkdir -p "$BUILD_DIR/bin"
+echo "build path: $(cd $BUILD_DIR; pwd)"
+cd "$PROJECT_DIR" || exit
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "$BUILD_DIR/bin/diss-backend"
+
+
+#
+#### 准备 build 文件
+echo "=========== 3. cp files for docker build ==========="
+cd $PROJECT_DIR
+cp -r conf swagger "$BUILD_DIR"
+cp entrypoint.sh script/install.sh "$BUILD_DIR"
+cp docker-compose-prod.yml "$BUILD_DIR/docker-compose.yml"
+
+#### 停止容器
 echo "=========== 4. stop diss-backend and db ==========="
+cd $BUILD_DIR
 docker-compose down
 
-#删除镜像
+
+#### 删除镜像
 echo "=========== 5. remove diss-backen images ==========="
-docker rmi `docker images | grep diss-backend | awk '{print $3}'` -f
-docker rmi `docker images -f "dangling=true" -q`
+DISS_BACKEND_ID=`docker images | grep diss-backend | awk '{print $3}'`
+echo $DISS_BACKEND_ID
+if [[ -n "$DISS_BACKEND_ID"  ]]
+then
+  docker rmi  -f $DISS_BACKEND_ID
+fi
+NONE_IMAGES_ID=`docker images -f "dangling=true" -q`
+if [[ -n "$NONE_IMAGES_ID"  ]]
+then
+  docker rmi -f NONE_IMAGES_ID
+fi
 
 # build 镜像
 echo "=========== 6. build diss-backend images ==========="
 docker-compose build --no-cache
 
-cd build
+cd $BUILD_DIR
 tar -cvf diss-backend.tar.gz ./*
 echo "=========== 7. build success ==========="
