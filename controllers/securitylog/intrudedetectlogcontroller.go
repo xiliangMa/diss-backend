48fd901af4126c7fddbfe12fa685116d9ca7f82a
xiliangMa <xiliangma@outlook.com>
2020-03-20 11:10:42 +0800

    add idls list api for security log
    Signed-off-by: xiliangMa <xiliangma@outlook.com>

--- controllers/securitylog/intrudedetectlogcontroller.go
+++ controllers/securitylog/intrudedetectlogcontroller.go
@@ -29,3 +29,17 @@ func (this *IntrudeDetectLogController) GetIntrudeDetectLogInfo() {
 	this.Data["json"] = intrudeDetectLog.List(0, intrudeDetectLog.Limit)
 	this.ServeJSON(false)
 }
+
+
+// @Title GetIntrudeDetectLog
+// @Description Get IntrudeDetectLog List
+// @Param token header string true "authToken"
+// @Param body body securitylog.IntrudeDetectLog false "入侵检测日志信息"
+// @Success 200 {object} models.Result
+// @router /idls [post]
+func (this *IntrudeDetectLogController) GetIntrudeDetectLogList() {
+	intrudeDetectLog := new(msl.IntrudeDetectLog)
+	json.Unmarshal(this.Ctx.Input.RequestBody, &intrudeDetectLog)
+	this.Data["json"] = intrudeDetectLog.List(0, intrudeDetectLog.Limit)
+	this.ServeJSON(false)
+}
