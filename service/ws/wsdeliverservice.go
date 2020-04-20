51bcfdb569a1e8b050797df8990cc45346d68e89
xiliangMa <xiliangma@outlook.com>
2020-04-20 15:21:38 +0800

    ws: add delete task api

--- service/ws/wsdeliverservice.go
+++ service/ws/wsdeliverservice.go
@@ -12,6 +12,7 @@ type WSDeliverService struct {
 	*Hub
 	Bath                 int64
 	CurrentBatchTaskList []*job.Task
+	DeleteTaskList       []*job.Task
 }
 
 func (this *WSDeliverService) DeliverTask() {
@@ -19,7 +20,7 @@ func (this *WSDeliverService) DeliverTask() {
 	for _, task := range this.CurrentBatchTaskList {
 		if _, ok := this.Hub.DissClient[task.Host.Id]; ok {
 			client := this.Hub.DissClient[task.Host.Id]
-			result := ws.WsData{Type: "Task", Tag: task.Type, Data: task, Config: ""}
+			result := ws.WsData{Type: ws.Type_Control, Tag: ws.Resource_Task, Data: task, RCType: ws.Resource_Control_Type_Post}
 			data, err := json.Marshal(result)
 			err = client.Conn.WriteMessage(websocket.TextMessage, data)
 			if err == nil {
@@ -33,4 +34,25 @@ func (this *WSDeliverService) DeliverTask() {
 		}
 	}
 	logs.Info("################ Deliver Task <<<end>>> ################")
+}
+
+func (this *WSDeliverService) DeleteTask() {
+	logs.Info("################ Delete Task <<<start>>> ################")
+	for _, task := range this.DeleteTaskList {
+		if _, ok := this.Hub.DissClient[task.Host.Id]; ok {
+			client := this.Hub.DissClient[task.Host.Id]
+			result := ws.WsData{Type: ws.Resource_Task, Tag: ws.Resource_Task, RCType: ws.Resource_Control_Type_Delete, Data: task}
+			data, err := json.Marshal(result)
+			err = client.Conn.WriteMessage(websocket.TextMessage, data)
+			if err == nil {
+				logs.Info("Delete Task Success, Id: %s, data: %v", task.Id, result)
+			} else {
+				logs.Error("Delete Task Fail, Id: %s, err: %s", task.Id, err.Error())
+			}
+		} else {
+			errMsg := "Agent not connect"
+			logs.Error("Delete Task Fail, Id: %s, err: %s", task.Id, errMsg)
+		}
+	}
+	logs.Info("################ Delete Task <<<end>>> ################")
 }
