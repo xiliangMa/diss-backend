0ca7b7ab6a886cdaf6416cc0b9d3d4a69d0c7644
xiliangMa <xiliangma@outlook.com>
2020-04-21 10:14:07 +0800

    ws: set task status to DeliverFailed when conn agent fail
    Signed-off-by: xiliangMa <xiliangma@outlook.com>

--- service/ws/wsdeliverservice.go
+++ service/ws/wsdeliverservice.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"github.com/astaxie/beego/logs"
 	"github.com/gorilla/websocket"
+	"github.com/xiliangMa/diss-backend/models"
 	"github.com/xiliangMa/diss-backend/models/job"
 	"github.com/xiliangMa/diss-backend/models/ws"
 )
@@ -22,14 +23,20 @@ func (this *WSDeliverService) DeliverTask() {
 		if _, ok := this.Hub.DissClient[task.Host.Id]; ok {
 			client := this.Hub.DissClient[task.Host.Id]
 			result := ws.WsData{Type: ws.Type_Control, Tag: ws.Resource_Task, Data: task, RCType: ws.Resource_Control_Type_Post}
-			data, err := json.Marshal(result)
-			err = client.Conn.WriteMessage(websocket.TextMessage, data)
+			data, _ := json.Marshal(result)
+			err := client.Conn.WriteMessage(websocket.TextMessage, data)
 			if err == nil {
 				logs.Info("Deliver Task Success, Id: %s, data: %v", task.Id, result)
 			} else {
+				//更新 task 状态
+				task.Status = models.Task_Status_Deliver_Failed
+				task.Update()
 				logs.Error("Deliver Task Fail, Id: %s, err: %s", task.Id, err.Error())
 			}
 		} else {
+			//更新 task 状态
+			task.Status = models.Task_Status_Deliver_Failed
+			task.Update()
 			errMsg := "Agent not connect"
 			logs.Error("Deliver Task Fail, Id: %s, err: %s", task.Id, errMsg)
 		}
