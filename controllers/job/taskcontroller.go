62435fb064be925f761bec03293d4df3bf3454df
xiliangMa <xiliangma@outlook.com>
2020-05-28 12:28:08 +0800

    nats: support delete task when task resource is container

--- controllers/job/taskcontroller.go
+++ controllers/job/taskcontroller.go
@@ -6,7 +6,7 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 	"github.com/xiliangMa/diss-backend/models"
-	"github.com/xiliangMa/diss-backend/service/ws"
+	"github.com/xiliangMa/diss-backend/service/nats"
 	"github.com/xiliangMa/diss-backend/utils"
 	"net/http"
 )
@@ -51,8 +51,8 @@ func (this *TaskController) DeleteTask() {
 			//向agent下发删除任务指令
 			deleteTaskList := data["items"]
 			deleteTask := deleteTaskList.([]*models.Task)[0]
-			WSDeliverService := ws.WSDeliverService{Hub: models.WSHub, DelTask: deleteTask}
-			err := WSDeliverService.DeleteTask()
+			natsSubService := nats.NatsSubService{DelTask: deleteTask}
+			err := natsSubService.DeleteTask()
 			if err == nil {
 				// 更新数据库状态 设置为删除锁定
 				deleteTask.Status = models.Task_Status_Removing
@@ -62,12 +62,21 @@ func (this *TaskController) DeleteTask() {
 				taskLog := models.TaskLog{RawLog: msg, Task: task, Level: models.Log_level_Info}
 				taskLog.Add()
 			} else {
-				result.Code = utils.DeleteTaskErr
-				result.Message = "DeleteTaskErr"
-				result.Data = nil
-				msg := fmt.Sprintf("Delet Task fail, Id: %s, err: %s", task.Id, result.Message)
-				taskLog := models.TaskLog{RawLog: msg, Task: task, Level: models.Log_level_Error}
-				taskLog.Add()
+				//如果操作资源不存在（无法给nats下发任务） 直接删除
+				if err.Error() == string(utils.ResourceNotFoundErr) {
+					task.Delete()
+					msg := fmt.Sprintf("Delete Task success, status: %s, Id: %s", deleteTask.Status, deleteTask.Id)
+					logs.Info(msg)
+					taskLog := models.TaskLog{RawLog: msg, Task: task, Level: models.Log_level_Info}
+					taskLog.Add()
+				} else {
+					result.Code = utils.DeleteTaskErr
+					result.Message = "DeleteTaskErr"
+					result.Data = nil
+					msg := fmt.Sprintf("Delet Task fail, Id: %s, err: %s", task.Id, result.Message)
+					taskLog := models.TaskLog{RawLog: msg, Task: task, Level: models.Log_level_Error}
+					taskLog.Add()
+				}
 			}
 		}
 	}
