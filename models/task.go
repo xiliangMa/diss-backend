d0eb599460851c58d3a411bab60f777f221b50db
SoAop <soaop@sina.cn>
2021-03-29 21:48:54 +0800

    task: fix search by host can not find container type task

--- models/task.go
+++ models/task.go
@@ -77,6 +77,7 @@ func (this *Task) List(from, limit int) Result {
 	}
 	if this.Host != nil && this.Host.Id != "" {
 		cond = cond.And("host_id", this.Host.Id)
+		cond = cond.Or("container_host_id", this.Host.Id)
 	}
 	if this.Container != nil && this.Container.Id != "" {
 		cond = cond.And("container_id", this.Container.Id)
