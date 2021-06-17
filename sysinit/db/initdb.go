7ff70dc3df13566579be388a050596f4b7863d45
SoAop <soaop@sina.cn>
2021-06-17 20:20:20 +0800

    dbconn: add GetConn common connect func

--- sysinit/db/initdb.go
+++ sysinit/db/initdb.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 	"github.com/astaxie/beego/orm"
@@ -20,20 +19,7 @@ func (this *DefaultDB) InitDB() {
 	// true: drop table 后再建表
 	force, _ := beego.AppConfig.Bool("Force")
 
-	//连接名称
-	//dbAlias := beego.AppConfig.String(DS + "::Alias")
-	//数据库名称
-	dbName := beego.AppConfig.String(DSAlias + "::DBName")
-	//数据库连接用户名
-	dbUser := beego.AppConfig.String(DSAlias + "::User")
-	//数据库连接密码
-	dbPwd := beego.AppConfig.String(DSAlias + "::Pwd")
-	//数据库IP（域名）
-	dbHost := beego.AppConfig.String(DSAlias + "::Host")
-	//端口
-	port := beego.AppConfig.String(DSAlias + "::Port")
-
-	DS := fmt.Sprintf("%s%s%s%s%s%s", "host="+dbHost, " port="+port, " user="+dbUser, " password="+dbPwd, " dbname="+dbName, " sslmode=disable")
+	DS := GetConn(DSAlias)
 	orm.RegisterDriver(driver, orm.DRPostgres)
 	err := orm.RegisterDataBase(DSAlias, driver, DS)
 	if err != nil {
